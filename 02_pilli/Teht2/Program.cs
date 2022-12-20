d9bdcb9fab9588dd55204506ee856c27beca8345
Milo Niiranen <AD4446@student.jamk.fi>
2022-12-20 13:29:46 +0200

    teht2 Pilli

---
+++ 02_pilli/Teht2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Teht2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pilli sorsapilli = new Pilli("Kvaak");
+            Pilli kukkopilli = new Pilli("Peef");
+            sorsapilli.Soi();
+            kukkopilli.Soi();
+            sorsapilli.Soi();
+            
+        }
+    }
+}
