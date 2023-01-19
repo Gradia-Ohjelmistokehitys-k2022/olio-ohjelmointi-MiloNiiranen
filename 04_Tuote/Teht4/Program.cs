f2e5b71e9dc652b52e8ebd46337d6c4ecd11dd4b
Milo Niiranen <AD4446@student.jamk.fi>
2023-01-19 10:19:21 +0200

    Teht4

---
+++ 04_Tuote/Teht4/Program.cs
@@ -0,0 +1,15 @@
+﻿using Teht4;
+
+namespace Teht4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tuote banaani = new Tuote(2.6, 3, "banaani");
+            Tuote omena = new Tuote(3.5, 2, "omena");
+            omena.TulostaTuote();
+            banaani.TulostaTuote();
+        }
+    }
+}
