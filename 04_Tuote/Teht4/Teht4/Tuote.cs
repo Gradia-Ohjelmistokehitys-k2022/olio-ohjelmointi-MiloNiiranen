24b9cf79c539f8af2af0854b05998e998ca16ca8
Milo <niiranenmilo@gmail.com>
2023-01-19 10:11:28 +0200

    Teht4

---
+++ 04_Tuote/Teht4/Teht4/Tuote.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Teht4
+{
+    internal class Tuote
+    {
+        double hintaAlussa;
+        int maaraAlussa;
+        string nimiAlussa;
+
+        public Tuote(double hintaAlussa, int maaraAlussa, string nimiAlussa)
+        {
+            this.hintaAlussa = hintaAlussa;
+            this.maaraAlussa = maaraAlussa;
+            this.nimiAlussa = nimiAlussa;
+        }
+
+        public void TulostaTuote()
+        {
+            Console.WriteLine(nimiAlussa + "hinta" + hintaAlussa + maaraAlussa + "kpl");
+        }
+    }
+}
