b60c020fff0222d29fb69734cac88a576615e895
Milo Niiranen <AD4446@student.jamk.fi>
2023-01-20 09:06:57 +0200

    Teht6

---
+++ 06_Musiikkikappale/Teht6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Teht6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musiikkikappale garden = new Musiikkikappale("Milo bite ", 10920);
+            Console.WriteLine("Kappaleen " + garden.Nimi() + " pituus on " + garden.Pituus() + " sekuntia.");
+
+        }
+    }
+}
