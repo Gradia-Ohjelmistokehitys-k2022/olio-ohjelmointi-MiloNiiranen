450069ecf5574b67713f4ee2158f82eb6f1ab1e2
Milo Niiranen <AD4446@student.jamk.fi>
2023-01-24 11:58:13 +0200

    Teht8

---
+++ 08_Maksukortti/Teht8/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Teht8
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Maksukortti Pekkakortti = new Maksukortti(20);
+            Maksukortti Mattikortti = new Maksukortti(30);
+
+            Pekkakortti.SyoMaukkaasti();
+            Mattikortti.SyoEdullisesti();
+
+            Console.WriteLine("Pekan saldo: " + Pekkakortti);
+            Console.WriteLine("Matin saldo: " + Mattikortti);
+
+            Pekkakortti.LataaRahaa(20);
+            Mattikortti.SyoMaukkaasti();
+
+            Console.WriteLine("Pekan saldo: " + Pekkakortti);
+            Console.WriteLine("Matin saldo: " + Mattikortti);
+
+            Pekkakortti.SyoEdullisesti();
+            Pekkakortti.SyoEdullisesti();
+            Mattikortti.LataaRahaa(50);
+
+            Console.WriteLine("Pekan saldo: " + Pekkakortti);
+            Console.WriteLine("Matin saldo: " + Mattikortti);
+
+        }
+    }
+}
