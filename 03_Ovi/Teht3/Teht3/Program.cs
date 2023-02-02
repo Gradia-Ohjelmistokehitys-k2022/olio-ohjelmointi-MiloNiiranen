ef087671e191b4a122edd8f7ce21f511678e31b7
Milo <niiranenmilo@gmail.com>
2023-02-02 08:51:41 +0200

    Teht

---
+++ 03_Ovi/Teht3/Teht3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Teht3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ovi alexander = new Ovi();
+            alexander.Koputa();
+            alexander.Koputa();
+        }
+    }
+}
---
+++ 08_Maksukortti/Teht8/Teht8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Teht8
+{
+     class Program
+    {
+
+         static void Main(string[] args)
+        {
+            Maksukortti kortti = new Maksukortti(5);
+            Console.WriteLine(kortti);
+            kortti.LataaRahaa(15);
+            Console.WriteLine(kortti);
+            kortti.LataaRahaa(10);
+            Console.WriteLine(kortti);
+            kortti.LataaRahaa(200);
+            Console.WriteLine(kortti);
+        }
+    }
+}
