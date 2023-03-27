c9296df93b3c5bd97dc0e1337a084fd3d2fe3ccf
Milo Niiranen <AD4446@student.jamk.fi>
2023-03-27 09:46:52 +0300

    10 Tehtävä

--- 10_OhjelmointipahkinaFizzBuzz/Elaintenlajittelija/Program.cs
+++ 10_OhjelmointipahkinaFizzBuzz/Elaintenlajittelija/Program.cs
@@ -1,45 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+    
+Console.WriteLine("Montako eläintä luodaan");
+int luku = int.Parse(Console.ReadLine());
 
-namespace RemainderElaimet
+
+for (int i = 0; i <= luku; i++)
 {
-    class Elaintenlajittelija
+    if (i % 3 == 0)
     {
-        static void Main(string[] args)
-        {
-
-            Vuoro();
-            Jakojaannos();
-
-        }
-
-
-        // 1. tapa. Tutki jakojaannös % (modulo) opetaaraattorin käyttöä ja mieti voisiko se auttaa tehtävät ratkaisussa.
-        // kun neljällä jaollinen, niin pitää olla hevonen
-        // 1 mod 3 = 1, koska 0 + 0 * 3 + m = 1, joten m = 1
-        // 2 mod 3 =2, koska 0 + 0 * 3 + m = 2, joten m = 2
-        private static void Jakojaannos()
-        {
-
-
-        }
-
-        // 2. Voit ajatella ongelmaa myös jonottamisen kautta. Eläimet pyörivät jonossa ja vaihtavat paikkaa. Ensimmäisenä ollut eläin
-        // joutuu aina jonon perälle (hevonen on aina viimeinen) eli kolmanneksi. Toinen eläin on ensimmäinen seuraavalal kierroksella ja 
-        // siirtyy jälleen kolmanneksi jne.
-        private static void Vuoro()
-        {
-            int elainVuoro = 1;
-
-            // Kissa, koira, papukaija, hevonen
-
-            // kun neljällä jaollinen, niin pitää olla hevonen
-            // 1 mod 3 = 1, koska 0 + 0 * 3 + m = 1, joten m = 1
-            // 2 mod 3 =2, koska 0 + 0 * 3 + m = 2, joten m = 2
-
-        }
+        Console.WriteLine("Kissa");
+    }
+    else if (i % 3 == 1)
+    {
+        Console.WriteLine("Koira");
+    }
+    else if (i % 3 == 2)
+    {
+        Console.WriteLine("Papukaija");
+    }
+    else if(i % 4 == 0)
+    {
+        Console.WriteLine("Hevonen");
     }
 }
+
